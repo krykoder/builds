1e3571f03bd31bce46a274495349b305622a563e
Igor Artamonov <igor@artamonov.ru>
2017-07-28 22:36:52 -0400

    code formatting

--- src/main.js
+++ src/main.js
@@ -45,120 +45,124 @@ function activateTab(el) {
 function init() {
 
 
-        // DOM elements
-        let $base = $('#build-items table tbody');
-        let $breadcrumbs = $("#project-breadcrumbs");
-        let $selectEmeraldWallet = $("#select-emerald-wallet");
-        let $selectGeth = $("#select-geth");
+    // DOM elements
+    let $base = $('#build-items table tbody');
+    let $breadcrumbs = $("#project-breadcrumbs");
+    let $selectEmeraldWallet = $("#select-emerald-wallet");
+    let $selectGeth = $("#select-geth");
 
-        function setProjectEndpoint(prefix) {
-                var s = gcpRequestSettings;
-                s["data"]["prefix"] = prefix;
-                return s;
+    function setProjectEndpoint(prefix) {
+        var s = gcpRequestSettings;
+        s["data"]["prefix"] = prefix;
+        return s;
+    }
+
+    function getBaseEndpoint(project) {
+        if (project.files.length > 0) {
+            return renderProjectFiles();
+        }
+        let allfiles = [];
+
+        function getArbitraryEndpoints(prefixes) {
+            let defer = $.Deferred();
+            let defers = [];
+            for (var i = 0; i < prefixes.length; i++) {
+                defers.push($.ajax(setProjectEndpoint(prefixes[i])));
+            }
+            $.when(...defers).done(function (...resArray) {
+                defer.resolve(resArray);
+            }).fail(function (errs) {
+                console.log(errs);
+                defer.reject(errs);
+            });
+            return defer.promise();
         }
 
-        function getBaseEndpoint(project) {
-                if (project.files.length > 0) {
-                        return renderProjectFiles();
+        function gotEndpoints(resArr) {
+            if (resArr[0].constructor !== Array) {
+                resArr = [resArr];
+            }
+            console.log("resArr", resArr);
+            let moarPrefixes = [];
+            for (var j = 0; j < resArr.length; j++) {
+                // 0: {kind: "storage#objects", prefixes: Array(1)}1: "success"2: {readyState: 4, getResponseHeader: ƒ, getAllResponseHeaders: ƒ, setRequestHeader: ƒ, overrideMimeType: ƒ, …}length: 3__proto__: Array(0)
+                let res = resArr[j];
+                if (res[1] !== "success") {
+                    console.log(res);
+                    return;
                 }
-                let allfiles = [];
-                function getArbitraryEndpoints(prefixes) {
-                        let defer = $.Deferred();
-                        let defers = [];
-                        for (var i = 0; i < prefixes.length; i++) {
-                                defers.push($.ajax(setProjectEndpoint(prefixes[i])));
-                        }
-                        $.when(...defers).done(function(...resArray) {
-                                defer.resolve(resArray);
-                        }).fail(function(errs) {
-                                console.log(errs);
-                                defer.reject(errs);
-                        });
-                        return defer.promise();
-                }
-
-                function gotEndpoints(resArr) {
-                        if (resArr[0].constructor !== Array) {
-                                resArr = [resArr];
-                        }
-                        console.log("resArr", resArr);
-                        let moarPrefixes = [];
-                        for (var j = 0; j < resArr.length; j++) {
-                                // 0: {kind: "storage#objects", prefixes: Array(1)}1: "success"2: {readyState: 4, getResponseHeader: ƒ, getAllResponseHeaders: ƒ, setRequestHeader: ƒ, overrideMimeType: ƒ, …}length: 3__proto__: Array(0)
-                                let res = resArr[j];
-                                if (res[1] !== "success") {
-                                        console.log(res);
-                                        return;
-                                }
-                                let response = res[0];
-                                console.log("prefixes", response.prefixes, "items", response.items);
-                                if (typeof response.items !== "undefined") {
-                                        let files = response.items.filter((item) => {
-                                                let isNested = item.name.split("/").length >= 2;
-                                                let isDir = item.name.endsWith("/");
-                                                return isNested && !isDir;
-                                        });
-                                        allfiles = allfiles.concat(files);
-                                }
-
-                                if (typeof response.prefixes !== "undefined" && response.prefixes.length > 0) {
-                                        let prefixes = response.prefixes.filter((prefix) => {
-                                                let basename = prefix.split("/")[0];
-                                                let pos = whitelistPrefixes.indexOf(basename);
-                                                return pos >= 0;
-                                        });
-                                        moarPrefixes = prefixes;
-                                }
-                        }
-                        if (moarPrefixes.length > 0) {
-                                getArbitraryEndpoints(moarPrefixes).then(gotEndpoints);
-                        } else {
-                                project.files = allfiles.sort((a, b) =>
-                                        moment(b.timeCreated) - moment(a.timeCreated)
-                                );
-                                renderProjectFiles();
-                        }
-                }
-                function renderProjectFiles() {
-                        $("#current-project-header").html(`${project.name} <small>Latest Artifacts</small>`);
-                        renderItems(project.files);
+                let response = res[0];
+                console.log("prefixes", response.prefixes, "items", response.items);
+                if (typeof response.items !== "undefined") {
+                    let files = response.items.filter((item) => {
+                        let isNested = item.name.split("/").length >= 2;
+                        let isDir = item.name.endsWith("/");
+                        return isNested && !isDir;
+                    });
+                    allfiles = allfiles.concat(files);
                 }
 
-                // Kickoff.
-                getArbitraryEndpoints([project.baseEndpoint])
-                        .then(gotEndpoints)
-                        .fail(function(e) {
-                                console.log(e);
-                        });
+                if (typeof response.prefixes !== "undefined" && response.prefixes.length > 0) {
+                    let prefixes = response.prefixes.filter((prefix) => {
+                        let basename = prefix.split("/")[0];
+                        let pos = whitelistPrefixes.indexOf(basename);
+                        return pos >= 0;
+                    });
+                    moarPrefixes = prefixes;
+                }
+            }
+            if (moarPrefixes.length > 0) {
+                getArbitraryEndpoints(moarPrefixes).then(gotEndpoints);
+            } else {
+                project.files = allfiles.sort((a, b) =>
+                    moment(b.timeCreated) - moment(a.timeCreated)
+                );
+                renderProjectFiles();
+            }
         }
 
-        // Input listeners
-        $selectEmeraldWallet.on("click", function(el) {
-                activateTab($selectEmeraldWallet);
-                getBaseEndpoint(projects.emeraldWallet);
-        });
-        $selectGeth.on("click", function(el) {
-                activateTab($selectGeth);
-                getBaseEndpoint(projects.geth);
-        });
-
-        function renderItems(files) {
-                if (typeof files === "undefined") { return; }
-                $base.html("");
-                files.map((item) => {
-                        let name = item.name.substr(item.name.lastIndexOf('/') + 1);
-                        let link = `/${item.name}`;
-                        let nameRow = `<td><a href="${link}">${name}</a></td>`;
-                        let size = filesize(item.size);
-                        let sizeRow = `<td>${size}</td>`;
-                        let time = moment(item.timeCreated);
-                        let timeRow = `<td>${time.format('lll')}</td>`;
-                        let row = `<tr>${sizeRow}${nameRow}${timeRow}</tr>`;
-                        $base.append(row)
-                });
+        function renderProjectFiles() {
+            $("#current-project-header").html(`${project.name} <small>Latest Artifacts</small>`);
+            renderItems(project.files);
         }
 
+        // Kickoff.
+        getArbitraryEndpoints([project.baseEndpoint])
+            .then(gotEndpoints)
+            .fail(function (e) {
+                console.log(e);
+            });
+    }
+
+    // Input listeners
+    $selectEmeraldWallet.on("click", function (el) {
+        activateTab($selectEmeraldWallet);
         getBaseEndpoint(projects.emeraldWallet);
+    });
+    $selectGeth.on("click", function (el) {
+        activateTab($selectGeth);
+        getBaseEndpoint(projects.geth);
+    });
+
+    function renderItems(files) {
+        if (typeof files === "undefined") {
+            return;
+        }
+        $base.html("");
+        files.map((item) => {
+            let name = item.name.substr(item.name.lastIndexOf('/') + 1);
+            let link = `/${item.name}`;
+            let nameRow = `<td><a href="${link}">${name}</a></td>`;
+            let size = filesize(item.size);
+            let sizeRow = `<td>${size}</td>`;
+            let time = moment(item.timeCreated);
+            let timeRow = `<td>${time.format('lll')}</td>`;
+            let row = `<tr>${sizeRow}${nameRow}${timeRow}</tr>`;
+            $base.append(row)
+        });
+    }
+
+    getBaseEndpoint(projects.emeraldWallet);
 }
 
 $(document).ready(init);
